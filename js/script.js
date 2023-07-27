93cee60bf47b55beb8bb3e50f1a972b8f1379e29
AlexCaturyty <olucascavalcanti@gmail.com>
2023-07-27 16:53:50 -0300

    Portfólio completo.

--- js/script.js
+++ js/script.js
@@ -21,5 +21,40 @@ ativaMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('ativado')
 })
 
-  
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+  const slideInterval = 4000; // Tempo de transição entre os slides em milissegundos
+
+  function showSlide(n) {
+    slides.forEach(slide => slide.style.display = "none");
+    slides[n].style.display = "block";
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  // Adicione a funcionalidade para os botões de navegação
+  document.querySelector(".prev").addEventListener("click", prevSlide);
+  document.querySelector(".next").addEventListener("click", nextSlide);
+
+  // Inicie o carrossel
+  showSlide(currentSlide);
+
+  // Defina o intervalo para avançar automaticamente
+  setInterval(nextSlide, slideInterval);
+});
+
+
+
+  
+  
